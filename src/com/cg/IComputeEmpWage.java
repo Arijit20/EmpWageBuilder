38887b253c15d896def053dd06b81ac72362bd75
Arijit20 <ARIJIT DEY@LAPTOP-F4J8SDAU>
2020-09-26 17:21:28 +0530

    [Arijit]Updated:Implemented Using Interface

---
+++ src/com/cg/IComputeEmpWage.java
@@ -0,0 +1,9 @@
+package com.cg;
+
+public interface IComputeEmpWage {
+	public void addCompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth);
+    public void computeEmpWage();
+    public int getTotalWage(String company);
+}
+
+
